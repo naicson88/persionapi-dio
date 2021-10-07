64a8236924f4b411a3ef236d987e1ac4ec61f0b3
Alan Naicson <naicson88@hotmail.com>
2021-10-06 22:32:16 -0300

    first

---
+++ src/main/java/com/naicson/dioproject/DioprojectApplication.java
@@ -0,0 +1,13 @@
+package com.naicson.dioproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/naicson/dioproject/DioprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naicson.dioproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
