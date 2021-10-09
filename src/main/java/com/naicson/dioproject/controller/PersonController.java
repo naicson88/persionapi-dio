755a6ef053724e5f88eb7824e8bef03474013546
a91628319 <naicson88@hotmail.com>
2021-10-09 20:14:35 -0300

    delete

--- src/main/java/com/naicson/dioproject/controller/PersonController.java
+++ src/main/java/com/naicson/dioproject/controller/PersonController.java
@@ -6,7 +6,9 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.naicson.dioproject.dto.MessageResponseDTO;
 import com.naicson.dioproject.dto.PersonDTO;
+import com.naicson.dioproject.exception.PersonNotFoundException;
 import com.naicson.dioproject.service.PersonService;
 
 @RestController
@@ -41,4 +44,14 @@ public class PersonController {
 	
 	}
 	
+	@GetMapping("/{id}")
+	public PersonDTO findById(@PathVariable Long id) throws PersonNotFoundException {
+		return service.findById(id);
+	}
+	
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void deleteById(@ PathVariable Long id) throws PersonNotFoundException {
+		service.delete(id);
+	}
 }
--- src/main/java/com/naicson/dioproject/service/PersonService.java
+++ src/main/java/com/naicson/dioproject/service/PersonService.java
@@ -1,6 +1,7 @@
 package com.naicson.dioproject.service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import com.naicson.dioproject.dto.MessageResponseDTO;
 import com.naicson.dioproject.dto.PersonDTO;
 import com.naicson.dioproject.entity.Person;
+import com.naicson.dioproject.exception.PersonNotFoundException;
 import com.naicson.dioproject.mapper.PersonMapper;
 import com.naicson.dioproject.repositry.PersonRepository;
 
@@ -41,5 +43,21 @@ public class PersonService {
 		return allPeople.stream()
 		.map(personMapper::toDTO)
 		.collect(Collectors.toList());
+	}
+
+
+	public PersonDTO findById(Long id) throws PersonNotFoundException {			
+		// Manda direto para o throw caso a consulta retorne vazia.
+	Person person =	personRepository.findById(id).orElseThrow(() -> new PersonNotFoundException(id));
+
+		return personMapper.toDTO(person);
+	}
+
+
+	public void delete(Long id) throws PersonNotFoundException {
+		personRepository.findById(id).orElseThrow(() ->  new PersonNotFoundException(id));
+		
+		personRepository.deleteById(id);
+		
 	}
 }
